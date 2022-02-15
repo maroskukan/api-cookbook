c3ed09f7d0424ef307eeba4ec035b91436b76eea
Maros Kukan <maros.kukan@me.com>
2022-02-15 18:00:02 +0100

    Add examples for requesting data using Javascript

---
+++ examples/javascript/requesting/request_fetch.js
@@ -0,0 +1,13 @@
+fetch('https://hplussport.com/api/products')
+    .then(
+        function (response)
+        {
+            return response.json();
+        }
+    )
+    .then(
+        function (respData)
+        {
+            console.log(respData)
+        }
+    )
