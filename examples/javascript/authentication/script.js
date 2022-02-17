970dabfe0bb23f113e240626d70aa864b485ac9b
Maros Kukan <maros.kukan@me.com>
2022-02-17 17:25:27 +0100

    Add parsing using dot notation and appending to html page

--- examples/javascript/authentication/script.js
+++ examples/javascript/authentication/script.js
@@ -1,12 +1,19 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=yourcustomapikeyvalue&tag=squirrels&rating=g');
+request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=yourcustomapikeyvalue&tag=squirrel&rating=g');
 
 request.onload = function ()
 {
     var response = request.response;
     var parsedData = JSON.parse(response);
     console.log(parsedData);
+
+    var originalUrl = parsedData.data.images.original.url;
+    console.log(originalUrl);
+
+    var gif = document.createElement('img')
+    gif.setAttribute('src', originalUrl);
+    document.body.appendChild(gif);
 }
 
 request.send()
